655b781ff4e9d3b84bbbfca9fc8310c7a37876c2
ppauloa37 <ppauloa37@gmail.com>
2025-02-21 16:10:17 -0300

    Update creat hljsdl.js

--- hljsdl.js
+++ hljsdl.js
@@ -1,1 +1,18 @@
-
+var cnvaan = document.getElementById("blcani");
+    var j = document.getElementById("j");
+    
+    
+    
+    
+    
+    j.innerHTML = "<h1>About today cats</h1>";
+    var animation = function() {
+        var newwdt = "10";
+        
+        
+        
+        j.style.width = newwdt + "px"
+        
+    }
+    window.requestAnimationFrame(animation);
+    animation();
